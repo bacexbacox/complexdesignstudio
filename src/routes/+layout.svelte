fddda69f0a5a02c9c05cbf2fc50f76acde15b566
Andrian Tarigan <andrian.tarigan@gmail.com>
2026-08-13 09:31:11 +0700

    Update site favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
 	<!-- Google Fonts: Inter -->
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
